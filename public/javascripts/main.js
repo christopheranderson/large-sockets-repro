59af7f0fbf83755c8269ea3bc6e460e5303f31ad
t0x3e8 <t0x3e8@gmail.com>
2016-04-22 22:11:01 +0200

    Required.js applied

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,21 @@
-var socket = io.connect(window.location.href)
-socket.on('servermessage', function (msg) {
-  var element = document.getElementById('random');
-  element.innerHTML = msg;
+require.config({
+  baseUrl: '/javascripts',
+  paths : {
+    'socket.io' : '//cdn.socket.io/socket.io-1.4.5' 
+  },
+  shim: {
+    'socket.io' : {
+      exports : 'io'
+    }
+  }
+});
+
+require(['socket.io', 'window', 'document'], function (io, win, doc) {
+  'use strict';
+
+  var socket = io.connect(win.location.href)
+  socket.on('servermessage', function (msg) {
+    var element = doc.getElementById('random');
+    element.innerHTML = msg;
+  });
 });
